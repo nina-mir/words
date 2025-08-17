52709ad844875b58a2b3510346e8a6b6376264bd
nina-mir <ninamirf@gmail.com>
2025-08-17 13:47:59 -0700

    dark theme added for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   console.log('pathname : ', location.pathname)
 
   return (
-    <div className='flex flex-col min-h-scr dark:bg-amber-900'>
+    <div className='flex flex-col  dark:bg-amber-900'>
       {/* <DarkModeTest/> */}
       {!isAuthPage && !isBioPage && <Header />}
 
@@ -37,17 +37,17 @@ function App() {
       </main>
 
       <div className='
-      flex flex-col items-center justify-center
+      flex flex-col items-center justify-center 
       '>
         <div className='flex flex-col w-full gap-5'>
-          <div>
-            <div className='h-[2rem] bg-pink-500 mb-[2rem]'>
+          <div className='break-auto hyphens-auto  dark:[*:not(h2)]:text-white dark:text-shadow-sky-400'>
+            <div className='dark:text-inherit h-[2rem] bg-pink-500 mb-[2rem]'>
 
-              <h2 className='mr-[10%] text-5xl font-p22 items-start mb-[1rem]'>
-                Current project:
+              <h2 className='dark:text-black px-1 mr-[10%] text-5xl font-p22 items-start mb-[1rem]'>
+                Current
               </h2>
             </div>
-            <p className='w-[90%] text-lg font-mono mb-[1rem] ml-[2rem]'>
+            <div className='w-[85%] text-lg font-mono mb-[1rem] ml-[2rem]'>
               <em>Taschen-Dolmetscher.Revisited</em><br />
               <span className={`
             font-header 
@@ -58,34 +58,53 @@ function App() {
             rounded-4xl
             dark:px-2
             px-1.5
-            `}>[multi-media, web-game]</span>:
-              <br/>A linguistic and historical journey through WW2 German-Russian phrases as a language learning game
-              inspired by
+            
+            `}>[multi-media, web-game]</span>
+              <br />A linguistic and historical journey through WW2 German-Russian phrases as a language learning game&nbsp;
+              <span className='font-semibold'>prompted by</span>
               <ul>
-                <li className='list-inside list-disc'>Ilya Ehrenburg & Vasily Grossman's words and reportage from&nbsp;
+                <li className='list-inside list-disc'>Wehrmacht-issued Russian translation booklet&nbsp;
                   <span className='border-b-2 border-b-yellow-300'>
-                    the Eastern Front (1941-1945)
+                    <a href='https://www.buchfreund.de/de/d/p/111627405/taschen-dolmetscher-fuer-frontsoldaten-russisch'>
+                      "Taschen-Dolmetscher für Frontsoldaten - Russisch (1941)"
+                    </a>
                   </span>
                 </li>
-                <li className='list-inside list-disc'>Artwork made by Soviet/Jewish partisans & survivors/witnesses of Holocaust</li>
-                <li className='list-inside list-disc'>The heroic/forgotten sacrifices of ordinary Soviet citizens in the face of the monstrous savagery of the German occupiers and the incompetency of Stalin's regime</li>
               </ul>
-            </p>
+              and <span className='font-semibold'>inspired by</span>
+              <ul>
+                <li className='list-inside list-disc'>Ilya Ehrenburg & Vasily Grossman's words from
+                  the Eastern Front (1941-1945)
+                </li>
+                <li className='list-inside list-disc'>Artwork made by Soviet/Jewish partisans & survivors/witnesses of Holocaust</li>
+                <li className='list-inside list-disc'>The heroic/forgotten sacrifices of ordinary Soviet citizens in the face of the monstrous savagery of the German occupiers and the incompetency of Stalin</li>
+              </ul>
+            </div>
             <div className='font-mono flex flex-col items-center gap-2 w-full'>
               <a
-                className='font-semibold bg-pink-300 rounded-4xl p-1 w-70 text-center'
+                className=' 
+                         
+                font-semibold bg-pink-500 rounded-4xl p-1 w-70 text-center
+                inset-shadow-xs
+                hover:bg-pink-600
+                inset-shadow-pink-500'
                 href='https://nina-mir.github.io/taschen-dolmetcher/'>
                 check it out 🔗
               </a>
-              <p className='font-semibold bg-pink-300 rounded-4xl p-1 w-70 text-center'>
-                <a href='https://github.com/nina-mir/taschen-dolmetcher'>
+              <p className='
+              inset-shadow-xs
+              inset-shadow-pink-500
+              hover:bg-pink-600
+              font-semibold bg-pink-500 rounded-4xl p-1 w-70 text-center'>
+                <a 
+                href='https://github.com/nina-mir/taschen-dolmetcher'>
                   contribute to the project <GitHubLogoIcon className='inline-block w-5 h-auto' />
                 </a>
               </p>
             </div>
           </div>
           <div className='h-[2rem] bg-pink-400 mb-[2rem]'>
-            <h2 className='mr-[10%] text-5xl font-p22 items-start mb-[1rem]'>Publications:</h2>
+            <h2 className='pl-1 mr-[10%] text-5xl font-p22 items-start mb-[1rem]'>Publications</h2>
           </div>
         </div>
         {boxes}
